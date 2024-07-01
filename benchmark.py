017ff52e11b42d64182cff48b445770ebe83ae60
Lim-Dul <limdul@gmail.com>
2024-07-01 23:29:33 +0200

    refactor

--- benchmark.py
+++ benchmark.py
@@ -5,32 +5,31 @@ from collections import Counter
 import cProfile
 from wonderwords import RandomWord
 
+text = []
 
-text = RandomWord().random_words(1000)
+def GenText():
+    """ Generate ONE MILLION words """
+    while len(text) < 1000000:
+        text.extend(RandomWord().random_words(1000))
+    print(len(text))
 
-for i in range(1000):
-    text.extend(RandomWord().random_words(1000))
-print(len(text))
+cProfile.run("GenText()")
 
-def test():
-    """ Blah """
-    strichliste = Counter("".join(text))
+def StrichlisteCounter():
+    """ Strichliste mit Counter """
+    strichliste = Counter("".join(text).lower())
     print(strichliste)
 
-def test2():
-    """ Blah """
-    # Meine Lösung 1
+def LoopCount():
+    """ Loop mit Count """
     text_lower = "".join(text).lower()
-    text_unique = "".join(set(text_lower))
-    print(text_unique)
+    text_unique = set(text_lower)
     char_list = []
+
     for character in text_unique:
         char_list.append([character, text_lower.count(character)])
-    # print(char_list)
     char_list_sorted = sorted(char_list, key=lambda x: x[1], reverse=True)
-    # print(char_list_sorted)
-    for char_count_pair in char_list_sorted:
-        print(char_count_pair[0] + ":", char_count_pair[1])
+    print(char_list_sorted)
 
-cProfile.run("test()")
-cProfile.run("test2()")
+cProfile.run("StrichlisteCounter()")
+cProfile.run("LoopCount()")
