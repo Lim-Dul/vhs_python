3c921d8936635d69d3a88b3acae867bab01403d6
Lim-Dul <limdul@gmail.com>
2024-07-01 14:43:05 +0200

    Better solutions for 2

--- m01_kennenlernen.py
+++ m01_kennenlernen.py
@@ -2,11 +2,13 @@
 Modul 01: Erste Variablen (Listen) und andere Übungen
 """
 # from statistics import mean
+from collections import Counter
 
 # Listen erkennen wir an eckigen Klammern. Listeneinträge werden durch Kommata getrennt.
 # Hier: Die Anweisung rechts vom Gleichzeitszeichen definiert eine Liste.
 # Durch die Gleichheitszeichen wird eine Variable vornamen definiert, der die Liste zugewiesen wird.
 # Daher heißt diese Anweisung auch "Zuweisung".
+
 vornamen = ["Felix", "Jan", "Christian", "Friederike", "Dirk", "Alina", "Tobias", "Dirk",
             "Irene", "Sabine", "Francisco", "Daniel"]
 # Hier: Wir legen eine Liste von Integer-Werten an.
@@ -50,24 +52,40 @@ print("Durchschnittsalter:", age_avg)
 # ---
 
 # 2) Häufigster Buchstabe im Vornamen
+# a) Vornamen mit for-Schleife zu einer einzigen Zeichenkette zusammensetzen
+alle_vornamen = ""
+for vorname in vornamen:
+    alle_vornamen += vornamen
 
-# ---
-# Meine Lösung 1
-first_names_concat_low = "".join(vornamen).lower()
-# print(first_names_concat)
-first_names_unique = "".join(set(first_names_concat_low))
-# print(first_names_unique)
-char_list = []
-for character in first_names_unique:
-    char_list.append([character, first_names_concat_low.count(character)])
-# print(char_list)
-char_list_sorted = sorted(char_list, key=lambda x: x[1], reverse=True)
-# print(char_list_sorted)
-for char_count_pair in char_list_sorted:
-    print(char_count_pair[0] + ":", char_count_pair[1])
-# ---
+strichliste = Counter(alle_vornamen)
+print(alle_vornamen)
+print(strichliste)
+
+# b) join-Methode
+# "".join(vornamen) fügt alle Listenelemente ohne Trennzeichen hintereinande
+strichliste = Counter("".join(vornamen))
+print(strichliste)
+
+# # ---
+# # Meine Lösung 1
+# first_names_concat_low = "".join(vornamen).lower()
+# # print(first_names_concat)
+# first_names_unique = "".join(set(first_names_concat_low))
+# # print(first_names_unique)
+# char_list = []
+# for character in first_names_unique:
+#     char_list.append([character, first_names_concat_low.count(character)])
+# # print(char_list)
+# char_list_sorted = sorted(char_list, key=lambda x: x[1], reverse=True)
+# # print(char_list_sorted)
+# for char_count_pair in char_list_sorted:
+#     print(char_count_pair[0] + ":", char_count_pair[1])
+# # ---
 
 # 3) Häufigkeit der Wohnorte
+strichliste = Counter(orte)
+print(strichliste)
+print(strichliste.most_common(1))
 
 # 4) Vornamen in alphabetisch sortierter Reihenfolge ausgeben
 print("vornamen aufsteigend sortiert:", sorted(vornamen))
