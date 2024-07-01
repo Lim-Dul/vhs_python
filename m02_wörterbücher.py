897a25ce31586b7c0e5209b2b2d16c4a0124404d
Lim-Dul <limdul@gmail.com>
2024-07-01 14:02:10 +0200

    Module 02 done

--- m02_wörterbücher.py
+++ m02_wörterbücher.py
@@ -14,4 +14,15 @@ print(de_uk["Tisch"])
 
 # Eingabe vom Anwender über die Tastatur entgegennehmen:
 wort_de = input("Bitte gib ein deutsches Wort ein: ")
-print("Englisch:", de_uk[wort_de])
+
+# 1) LBYL: Look Before You Leap
+if wort_de in de_uk: # Check: Ist wort_de ein Schlüssel im Wörterbuch de_uk?
+    print("Englisch:", de_uk[wort_de])
+else: # else = sonst
+    print("Leider gibt es für das Wort keine Übersetzung im Wörterbuch.")
+
+# 2) EAFP: It's Easier to Ask for Forgiveness than for Permission
+try:
+    print("Englisch:", de_uk[wort_de])
+except KeyError:
+    print("Leider gibt es für das Wort keine Übersetzung im Wörterbuch.")
