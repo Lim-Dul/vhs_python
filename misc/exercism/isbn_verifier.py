b9c2e671ad17d5338686abaa2277b3bd448c7a1f
Lim-Dul <limdul@gmail.com>
2024-07-03 01:31:33 +0200

    Solution for ISBN and docstrings

--- misc/exercism/isbn_verifier.py
+++ misc/exercism/isbn_verifier.py
@@ -1,11 +1,36 @@
+"""
+Exercism: ISBN
+"""
+
+
 def is_valid(isbn):
+    """
+    Prüft, ob eine valide ISBN-10-Nummer eingetragen wurde.
+    Hierzu wird eine Checksumme aus allen Zahlen gebildet.
+
+    Arguments:
+        isbn -- Nummer im ISBN-10-Format
+
+    Returns:
+        Wahr, wenn Nummer valide
+    """
     zahlen = list(isbn.replace("-", ""))
-    if len(zahlen)!=10:
+    if len(zahlen) != 10:
         return False
     if zahlen[-1] == "X":
         zahlen[-1] = "10"
     if not all(zahl.isdigit() for zahl in zahlen):
         return False
-    return sum(int(x) * y for x, y in zip(zahlen, range(10, 0, -1))) % 11 == 0
+    print(zahlen)
+    checksumme = 0
+    for i in reversed(range(len(zahlen))):
+        checksumme = checksumme + int(zahlen[i]) * (10 - i)
+        # print("Multiplying", int(zahlen[i]), "by", (10 - i), "equals", int(zahlen[i]) * (10 - i))
+    if checksumme % 11 == 0:
+        return True
+    return False
+
+    # return sum(int(x) * y for x, y in zip(zahlen, range(10, 0, -1))) % 11 == 0
 
 print(is_valid("3-598-21508-8"))
+print(is_valid("3-598-21515-X"))
--- misc/exercism/leap.py
+++ misc/exercism/leap.py
@@ -1,2 +1,23 @@
+"""
+Exercism: Leap Year
+"""
+
+
 def leap_year(year):
-    pass
+    """
+    Prüft, ob ein Jahr ein Schaltjahr ist.
+
+    Arguments:
+        year -- integer: Jahr
+
+    Returns:
+        Wahr, wenn Jahr ein Mehrfaches von 4,
+        außer wenn Mehrfaches von 100, außer wenn auch ein Mehrfaches von 400
+    """
+    if year % 400 == 0: # spezifischster Check zuerst
+        return True
+    if year % 100 == 0: # 100 als Sonderfall von 400
+        return False
+    if year % 4 == 0: # 4 als generischster Check
+        return True
+    return False
