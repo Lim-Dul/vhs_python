d3ab3ae9f94fefc9a23ada7df828cc4b68313cf2
Lim-Dul <limdul@gmail.com>
2024-07-03 01:33:00 +0200

    Comments

--- misc/exercism/leap.py
+++ misc/exercism/leap.py
@@ -14,10 +14,10 @@ def leap_year(year):
         Wahr, wenn Jahr ein Mehrfaches von 4,
         außer wenn Mehrfaches von 100, außer wenn auch ein Mehrfaches von 400
     """
-    if year % 400 == 0: # spezifischster Check zuerst
+    if year % 400 == 0: # seltenster Fall zuerst
         return True
     if year % 100 == 0: # 100 als Sonderfall von 400
         return False
-    if year % 4 == 0: # 4 als generischster Check
+    if year % 4 == 0: # 4 als häufigster Fall
         return True
     return False
