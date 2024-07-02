7a87e1d109fa8df41b296bd75668ae45f9c31180
Lim-Dul <limdul@gmail.com>
2024-07-02 15:58:35 +0200

    Pangram solution

--- misc/exercism/pangram.py
+++ misc/exercism/pangram.py
@@ -1,2 +1,30 @@
+"""
+Exercism Pangram
+"""
+import string
+
 def is_pangram(sentence):
-    pass
+    """
+    Schaut, ob in einem Satz jeder Buchstabe mindestens einmal vorkommt.
+
+    Arguments:
+        sentence -- String: Der Satz, der geprüft werden soll.
+
+    Returns:
+        Wahr, wenn Satz ein Pangramm ist, sonst Falsch
+    """
+    sentence = sentence.lower()
+
+    checker = set()
+
+    buchstaben = string.ascii_lowercase
+    for buchstabe in buchstaben:
+        if buchstabe in sentence:
+            checker.add(buchstabe)
+    if len(checker) == len(buchstaben):
+        return True
+
+    return False
+
+
+is_pangram("The quick brown fox jumps over the lazy fox.")
