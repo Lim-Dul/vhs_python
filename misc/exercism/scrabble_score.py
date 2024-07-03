69d6ecefe2080b105ebe21d50fe52991739ee055
Lim-Dul <limdul@gmail.com>
2024-07-03 18:10:45 +0200

    Solution to scrabble

--- misc/exercism/scrabble_score.py
+++ misc/exercism/scrabble_score.py
@@ -1,2 +1,27 @@
-def score(word):
-    pass
+"""
+Übung Scrabble aus Exercism
+"""
+punkte = {
+        'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+        'D': 2, 'G': 2,
+        'B': 3, 'C': 3, 'M': 3, 'P': 3,
+        'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+        'K': 5,
+        'J': 8, 'X': 8,
+        'Q': 10, 'Z': 10
+        }
+
+def score(wort):
+    """
+    Bewertet wort anhand der obigen Punktetabelle (Wörterbuch)
+
+    Arguments:
+        wort -- String: Wort
+
+    Returns:
+        Scrabble-Bewertung des Wortes
+    """
+    # Loop über zeichen in wort (Großbuchstaben), Lookup des von zeichen in Wörterbuch, dann Summe
+    return sum(punkte[zeichen] for zeichen in wort.upper())
+
+print(score("Eggplant"))
