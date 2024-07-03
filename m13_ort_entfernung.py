fd42bdd47bc324fad3540605be4d48c018570c03
Lim-Dul <limdul@gmail.com>
2024-07-03 14:57:18 +0200

    Finished calculating

--- m13_ort_entfernung.py
+++ m13_ort_entfernung.py
@@ -1,11 +1,27 @@
 from geopy.geocoders import Nominatim
+from geopy import distance
 
 
 orte = ["Bad Salzuflen", "Dietzenbach", "Bielefeld", "Bielefeld", "Spenge", "Bielefeld",
         "Bielefeld", "Schloss Holte-Stukenbrock", "Berlin", "Hannover", "Bielefeld", "Bielefeld"]
 
 orte = set(orte)
-ziel = "VHS Bielefeld"
+ziel = "Bielefeld"
+orte.remove(ziel)
 
 # Frage: Welcher Ort aus der Menge der orte
 # ist am weitesten vom ziel "VHS Bielefeld" entfernt (Luftlinie)?
+geolocator = Nominatim(user_agent="vhs-bielefeld-platz11")
+location = geolocator.geocode(ziel)
+latlong_ziel = (location.latitude, location.longitude)
+# print(latlong_ziel)
+
+distanz_orte = []
+for ort in orte:
+    location = geolocator.geocode(ort)
+    latlong_ort = (location.latitude, location.longitude)
+    distanz_ort = distance.distance(latlong_ort, latlong_ziel).km
+    distanz_orte.append((distanz_ort, ort))
+
+print(distanz_orte)
+print(max(distanz_orte))
