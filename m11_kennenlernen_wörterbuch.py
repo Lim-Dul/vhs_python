04a5aea1f304d6bfdd715be90e5d504fb1efce0c
Lim-Dul <limdul@gmail.com>
2024-07-03 09:23:59 +0200

    Initial commit module 11+ (OOP)

---
+++ m11_kennenlernen_wörterbuch.py
@@ -0,0 +1,14 @@
+"""
+Modul 11: Wörterbuch als Zwischenschritt zu OOP
+"""
+
+
+vornamen = ["Felix", "Jan", "Christian", "Friederike", "Dirk", "Alina", "Tobias", "Dirk",
+            "Irene", "Sabine", "Francisco", "Daniel"]
+altersangaben = [29, 40, 59, 35, 31, 27, 32, 51, 44, 35, 47, 41]
+orte = ["Bad Salzuflen", "Dietzenbach", "Bielefeld", "Bielefeld", "Spenge", "Bielefeld",
+        "Bielefeld", "Schloss Holte-Stukenbrock", "Berlin", "Hannover", "Bielefeld", "Bielefeld"]
+
+
+t1 = {"vorname": "Felix", "alter": 29, "ort": "Bad Salzuflen"}
+t2 = {"vorname": "Jan", "alter": 40, "ort": "Dietzenbach"}
