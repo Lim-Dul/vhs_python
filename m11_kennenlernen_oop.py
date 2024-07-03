ba62338e535ac34caf4b906ba6460052b966ccd0
Lim-Dul <limdul@gmail.com>
2024-07-03 11:46:18 +0200

    durchschnittsalter three ways

--- m11_kennenlernen_oop.py
+++ m11_kennenlernen_oop.py
@@ -2,6 +2,7 @@
 Modul 11: Teilnehmer jetzt mit Objekten
 """
 from datetime import date
+from statistics import mean
 
 class Teilnehmer:
     """
@@ -51,9 +52,23 @@ for _ in teilnehmerliste:
     print(_.alter())
 
 # Berechne das Durchschnittsalter der Teilnehmer in der teilnehmerliste:
+# a) for-Schleife
 summe_alter = 0
-anzahl_teilnehmer = 0
+anzahl_teilnehmer = len(teilnehmerliste)
 for teilnehmer in teilnehmerliste:
     summe_alter += teilnehmer.alter()
     durchschnitt_alter = summe_alter / anzahl_teilnehmer
 print(durchschnitt_alter)
+
+
+# b) Zwischenüberlegung: Wie bekomme ich eine Liste der Altersangaben?
+altersangaben = []
+for teilnehmer in teilnehmerliste:
+    altersangaben.append(teilnehmer.alter())
+
+print(sum(altersangaben) / len(altersangaben))
+
+# c) List comprehension (und mean)
+altersangaben = [teilnehmer.alter() for teilnehmer in teilnehmerliste]
+
+print(mean(altersangaben))
